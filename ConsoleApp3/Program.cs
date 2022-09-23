b18f76c3995db378ea12709f2273ab8a51d6733f
Begginnerrr <52282504+Begginnerrr@users.noreply.github.com>
2022-09-23 21:37:16 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+using ConsoleApp3;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using static K4os.Compression.LZ4.Engine.Pubternal;
+using Microsoft.EntityFrameworkCore.Design;
+using MySql.EntityFrameworkCore.Extensions;
+
+
+
+//MysqlEntityFrameworkDesignTimeServices abc = new MysqlEntityFrameworkDesignTimeServices();
+
+Console.WriteLine("Hello, World!");
+/*
+IHost hostBuilder = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        // var connectionString = hostContext.Configuration.GetConnectionString("server=localhost;user=root;database=SchoolDb;password=root;port=3306");
+       // var connectionString = context.Configuration.GetConnectionString("server=localhost;user=root;database=SchoolDb;password=root;port=3306");
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var cns = context.Configuration.GetConnectionString("server=localhost;user=root;database=SchoolDb;password=root;port=3306");
+        services.AddDbContext<SchoolContext>(options => options.UseMySQL("server=localhost;user=root;database=SchoolDb;password=root;port=3306"));
+       
+    })
+    .Build();
+hostBuilder.Run();
+*/
+Console.WriteLine("Maybe this will be reached??");
+
+Request request = new Request();
+request.refreshData();
+
+/*
+
+IHost host = Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((config) =>
+{
+   // config.AddJsonFile("appsettings.json");
+    config.AddEnvironmentVariables();
+    config.Build();
+})
+
+.ConfigureServices((context, services) =>
+{
+    var cns = context.Configuration.GetConnectionString("MyConnection");
+    services.AddDbContext<SchoolContext>(options => options.UseMySQL("server=localhost;user=root;database=SchoolDb;password=root;port=3306"));
+    //.AddHostedService<DiBerieBotMain>();
+    ;
+})
+.Build();
+*/
+
+
---
+++ ConsoleApp3/SchoolContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class SchoolContext : DbContext
+    { 
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+             
+        }
+
+        public SchoolContext()
+        {
+        }
+
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySQL("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Chinook");
+            }
+        }
+        */
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySQL(
+                    "server=localhost;user=root;database=SchoolDb;password=metadata1;port=3306");
+        }
+
+        //  protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source=AppDB.db");
+
+    }
+}
+ 
